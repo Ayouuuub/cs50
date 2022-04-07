e38858100469e1fa5d28d9683a636d7b00e486a9
Ayouuuub <62658586+Ayouuuub@users.noreply.github.com>
2022-04-07 22:37:54 +0000

    Thu, Apr 7, 2022, 10:37 PM +00:00

--- cash/test.c
+++ cash/test.c
@@ -20,19 +20,19 @@ int main(void)
     int dimes = calculate_dimes(cents);
     cents = cents - dimes * 10;
 
-    // // Calculate the number of nickels to give the customer
-    // int nickels = calculate_nickels(cents);
-    // cents = cents - nickels * 5;
+    // Calculate the number of nickels to give the customer
+    int nickels = calculate_nickels(cents);
+    cents = cents - nickels * 5;
 
-    // // Calculate the number of pennies to give the customer
-    // int pennies = calculate_pennies(cents);
-    // cents = cents - pennies * 1;
+    // Calculate the number of pennies to give the customer
+    int pennies = calculate_pennies(cents);
+    cents = cents - pennies * 1;
 
-    // // Sum coins
-    // int coins = quarters + dimes + nickels + pennies;
+    // Sum coins
+    int coins = quarters + dimes + nickels + pennies;
 
-    // // Print total number of coins to give the customer
-    // printf("%i\n", coins);
+    // Print total number of coins to give the customer
+    printf("%i\n", coins);
 }
 
 int get_cents(void)
@@ -63,29 +63,31 @@ int calculate_dimes(int cents)
         if (tmp >= 10) {
         calculate_dimes = tmp / 10 ;
             }
-            tmp = tmp - calculate_dimes * 10 ;
     return calculate_dimes;
 }
 
 int calculate_nickels(int cents)
 {
-    // TODO
     int cc = calculate_quarters(cents);
-    int cd = calculate_dimes(cents)
+    int cd = calculate_dimes(cents);
         int tmp = cents - cc * 25 - cd * 10;
         int calculate_nickels = 0;
             if (tmp >= 5) {
         calculate_nickels = tmp / 5 ;
             }
-            tmp = tmp - calculate_nickels * 5 ;
     return calculate_nickels;
 }
 
-// int calculate_pennies(int cents)
-// {
-//     // TODO
-//             if (tmp >= 1) {
-//         calculate_pennies = tmp ;
-//             }
-//     return calculate_pennies;
-// }
+    int calculate_pennies(int cents)
+    {
+        // TODO
+        int cc = calculate_quarters(cents);
+    int cd = calculate_dimes(cents);
+    int cn = calculate_nickels();
+        int tmp = cents - cc * 25 - cd * 10 - cn * 5;
+        int calculate_pennies = 0;
+                if (tmp >= 1) {
+            calculate_pennies = tmp ;
+                }
+        return calculate_pennies;
+    }
